164ea603149f33b8b830e2030202f1c5384db7b2
Rilka95 <159856705+Rilka95@users.noreply.github.com>
2024-03-02 20:01:04 -0700

    1.2 App.js

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {
+  BrowserRouter as Router, // Import BrowserRouter and rename it to Router for convenience
   Routes,
   Route
 } from 'react-router-dom';
 
-import IndexPage from 'pages.Index.js';
+import IndexPage from './pages/Index.js';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<IndexPage />} />
-    </Routes>
+    <Router> {/* Wrap Routes with Router */}
+      <Routes>
+        <Route path="/" element={<IndexPage />} /> {/* Ensure IndexPage is passed as an element */}
+      </Routes>
+    </Router>
   );
 }
 
